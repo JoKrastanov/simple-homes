75aa818f67bc0d7de0f385070ab3ae07919b9ec7
Joan <jokrastanov@gmail.com>
2021-09-15 11:02:08 +0200

    Finished property controller class

--- Simple_Homes/src/main/java/com/example/Simple_Homes/controller/PropertyController.java
+++ Simple_Homes/src/main/java/com/example/Simple_Homes/controller/PropertyController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.xml.ws.Response;
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -64,6 +65,33 @@ public class PropertyController {
             String url = "properties/" + property.getId();
             URI uri =URI.create(url);
             return new ResponseEntity(uri, HttpStatus.CREATED);
+        }
+    }
+
+    @PutMapping()
+    public ResponseEntity<Property> updateProperty(@RequestBody Property property)
+    {
+        if(testData.updateProperty(property)) {
+            return ResponseEntity.noContent().build();
+        }
+        else {return new ResponseEntity("Please provide a valid id.", HttpStatus.NOT_FOUND);}
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Property> updateProperty(@PathVariable("id") int id, @RequestParam("type") String type, @RequestParam("price") double price, @RequestParam("address") String address, @RequestParam("postalCode") String postalCode, @RequestParam("city") String city, @RequestParam("size") int size, @RequestParam("DateAvailable") Date dateAvailable, @RequestParam("Availability") boolean available)
+    {
+        Property property = testData.getProperty(id);
+        if (property == null)
+        {return new ResponseEntity("Property ID is invalid", HttpStatus.NOT_FOUND);}
+        else {
+            property.setType(type);
+            property.setDateAvailable(dateAvailable);
+            property.setSize(size);
+            property.setPrice(price);
+            property.setCity(city);
+            property.setAvailability(available);
+            property.setAddress(address);
+            return ResponseEntity.noContent().build();
         }
     }
 
