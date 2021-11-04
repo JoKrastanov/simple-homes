f3a943b9dc209ac669682ee2e3977b571d98a16d
Joan <jokrastanov@gmail.com>
2021-11-04 15:41:15 +0100

    Sonar Cube instalation

--- Simple_Homes/src/test/java/com/example/Simple_Homes/PropertyTest.java
+++ Simple_Homes/src/test/java/com/example/Simple_Homes/PropertyTest.java
@@ -13,7 +13,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PropertyTest {
     String DEFAULT_TYPE = "Room";
     double DEFAULT_PRICE = 400;
-    boolean taken = false;
     String DEFAULT_DESCRIPTION = "desc";
     boolean forSale = false;
     Long DEFAULT_PUBLISHERID = 1L;
@@ -21,9 +20,10 @@ public class PropertyTest {
     String DEFAULT_POSTACODE = "5656AB";
     String DEFAULT_CITY = "Eindhoven";
     int DEFAULT_SIZE = 12;
-    Date available = new Date(2021, 12, 24);
     int DEFAULT_ROOMS = 1;
     String DEFAULT_INTERIOR = "Furnished";
+
+    Property Test_Property = new Property(DEFAULT_TYPE, DEFAULT_PRICE, DEFAULT_PUBLISHERID, DEFAULT_ADDRESS, DEFAULT_POSTACODE, DEFAULT_CITY, DEFAULT_SIZE, "available", DEFAULT_DESCRIPTION, forSale, DEFAULT_ROOMS, DEFAULT_INTERIOR, "test");
 
     @Test
     public void TestConstructorSuccessful()
@@ -34,6 +34,15 @@ public class PropertyTest {
         assertEquals(DEFAULT_TYPE, property.getType());
         assertEquals(DEFAULT_ROOMS, property.getRooms());
 
+    }
+
+    @Test
+    public void TestSetTypeSuccessfull()
+    {
+        String newType = "House";
+        Test_Property.setType(newType);
+
+        assertEquals(newType, Test_Property.getType());
     }
 
 
