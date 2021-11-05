9d487176d56b99b496082adf800b4886877ee275
Joan <jokrastanov@gmail.com>
2021-11-05 13:09:37 +0100

    More Unit Tests

--- Simple_Homes/src/test/java/com/example/Simple_Homes/PropertyTest.java
+++ Simple_Homes/src/test/java/com/example/Simple_Homes/PropertyTest.java
@@ -1,6 +1,7 @@
 package com.example.Simple_Homes;
 
 import com.example.Simple_Homes.classes.Property;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.internal.matchers.Equals;
 import org.springframework.test.context.TestConstructor;
@@ -37,13 +38,159 @@ public class PropertyTest {
     }
 
     @Test
-    public void TestSetTypeSuccessfull()
+    public void TestSetTypeSuccessful()
     {
         String newType = "House";
         Test_Property.setType(newType);
 
         assertEquals(newType, Test_Property.getType());
     }
+    @Test
+    public void TestSetTypeUnsuccessful()
+    {
+        String newType = "House";
+        Test_Property.setType(newType);
+
+        Assertions.assertNotEquals(DEFAULT_TYPE, Test_Property.getType());
+    }
+
+    @Test
+    public void TestSetPriceSuccessful()
+    {
+        Double newPrice = 500D;
+        Test_Property.setPrice(newPrice);
+
+        assertEquals(newPrice, Test_Property.getPrice());
+    }
+    @Test
+    public void TestSetPriceUnsuccessful()
+    {
+        Double newPrice = 500D;
+        Test_Property.setPrice(newPrice);
+
+        Assertions.assertNotEquals(DEFAULT_PRICE, Test_Property.getPrice());
+    }
+
+    @Test
+    public void TestSetDescSuccessful()
+    {
+        String newDesc = "new desc";
+        Test_Property.setDescription(newDesc);
+
+        assertEquals(newDesc, Test_Property.getDescription());
+    }
+    @Test
+    public void TestSetDescUnsuccessful()
+    {
+        String newDesc = "new desc";
+        Test_Property.setDescription(newDesc);
+
+        Assertions.assertNotEquals(DEFAULT_DESCRIPTION, Test_Property.getDescription());
+    }
+
+    @Test
+    public void TestSetAddressSuccessful()
+    {
+        String newAddress = "Johannes van der Waalsweg";
+        Test_Property.setAddress(newAddress);
+
+        assertEquals(newAddress, Test_Property.getAddress());
+    }
+    @Test
+    public void TestSetAddressUnsuccessful()
+    {
+        String newAddress = "Johannes van der Waalsweg";
+        Test_Property.setAddress(newAddress);
+
+        Assertions.assertNotEquals(DEFAULT_ADDRESS, Test_Property.getAddress());
+    }
+
+    @Test
+    public void TestSetPostalCodeSuccessful()
+    {
+        String newPostalCode = "1234AB";
+        Test_Property.setPostalCode(newPostalCode);
+
+        assertEquals(newPostalCode, Test_Property.getPostalCode());
+    }
+    @Test
+    public void TestSetPostalCodeUnsuccessful()
+    {
+        String newPostalCode = "1234AB";
+        Test_Property.setPostalCode(newPostalCode);
+
+        Assertions.assertNotEquals(DEFAULT_POSTACODE, Test_Property.getPostalCode());
+    }
+
+    @Test
+    public void TestSetCitySuccessful()
+    {
+        String newCity = "Amsterdam";
+        Test_Property.setCity(newCity);
+
+        assertEquals(newCity, Test_Property.getCity());
+    }
+    @Test
+    public void TestSetCityUnsuccessful()
+    {
+        String newCity = "Amsterdam";
+        Test_Property.setCity(newCity);
+
+        Assertions.assertNotEquals(DEFAULT_CITY, Test_Property.getCity());
+    }
+
+    @Test
+    public void TestSetSizeSuccessful()
+    {
+        Integer newSize = 16;
+        Test_Property.setSize(newSize);
+
+        assertEquals(newSize, Test_Property.getSize());
+    }
+    @Test
+    public void TestSetSizeUnsuccessful()
+    {
+        Integer newSize = 16;
+        Test_Property.setSize(newSize);
+
+        Assertions.assertNotEquals(DEFAULT_SIZE, Test_Property.getSize());
+    }
+
+    @Test
+    public void TestSetRoomsSuccessful()
+    {
+        Integer newRooms = 2;
+        Test_Property.setRooms(newRooms);
+
+        assertEquals(newRooms, Test_Property.getRooms());
+    }
+    @Test
+    public void TestSetRoomsUnsuccessful()
+    {
+        Integer newRooms = 2;
+        Test_Property.setRooms(newRooms);
+
+        Assertions.assertNotEquals(DEFAULT_ROOMS, Test_Property.getRooms());
+    }
+
+    @Test
+    public void TestSetInteriorSuccessful()
+    {
+        String newInterior = "Unfurnished";
+        Test_Property.setInterior(newInterior);
+
+        assertEquals(newInterior, Test_Property.getInterior());
+    }
+    @Test
+    public void TestSetInteriorUnsuccessful()
+    {
+        String newInterior = "Unfurnished";
+        Test_Property.setInterior(newInterior);
+
+        Assertions.assertNotEquals(DEFAULT_INTERIOR, Test_Property.getInterior());
+    }
+
+
 
 
 }
