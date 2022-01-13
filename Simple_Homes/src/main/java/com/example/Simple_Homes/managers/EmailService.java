2e858941eb82ebb6123a5d6a96f09c6f3b480b4c
Joan <jokrastanov@gmail.com>
2022-01-13 14:03:32 +0100

    yes

---
+++ Simple_Homes/src/main/java/com/example/Simple_Homes/managers/EmailService.java
@@ -0,0 +1,7 @@
+package com.example.simple_homes.managers;
+
+
+public class EmailService {
+    private String email = "simplehomesofficial.info@gmail.com";
+    private String password = "simplehomes01";
+}
